e44378fd9e06e84c9026eec3d1527286e24a24c8
Vlad Klinov <klinowlad@gmail.com>
2022-03-29 16:45:48 +0300

    Some changes

--- app/src/main/java/com/klinovvlad/task1klinov/Fragments/FirstScreen.kt
+++ app/src/main/java/com/klinovvlad/task1klinov/Fragments/FirstScreen.kt
@@ -1,16 +1,18 @@
 package com.klinovvlad.task1klinov.Fragments
 
-import android.content.Intent
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.klinovvlad.task1klinov.Activities.MainActivity
 import com.klinovvlad.task1klinov.Activities.SecondActivity
 import com.klinovvlad.task1klinov.Adapter.MainAdapter
+import com.klinovvlad.task1klinov.Fragments.Communacation.Communicator
 import com.klinovvlad.task1klinov.Model.Item
 import com.klinovvlad.task1klinov.R
 import com.klinovvlad.task1klinov.databinding.FirstScreenBinding
@@ -20,6 +22,10 @@ class FirstScreen : Fragment(), MainAdapter.OnItemClickListener {
     lateinit var _adapter: MainAdapter
     lateinit var linerLayoutManager: LinearLayoutManager
     private var item_list_main: ArrayList<Item> = ArrayList()
+    private lateinit var communicator: Communicator
+    lateinit var shared_id : SharedPreferences
+    lateinit var shared_name : SharedPreferences
+    lateinit var shared_description : SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +38,26 @@ class FirstScreen : Fragment(), MainAdapter.OnItemClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?): Unit = with(first_screen_binding) {
         super.onViewCreated(view, savedInstanceState)
         val id_list = listOf(
-            Item(0, "name", "description"),
-            Item(1, "name1", "description1")
+            Item(0, "name0", "description0"),
+            Item(1, "name1", "description1"),
+            Item(2, "name2", "description2"),
+            Item(3, "name3", "description3"),
+            Item(4, "name4", "description4"),
+            Item(5, "name5", "description5"),
+            Item(6, "name6", "description6"),
+            Item(7, "name7", "description7"),
+            Item(8, "name8", "description8"),
+            Item(9, "name9", "description9"),
+            Item(10, "name10", "description10"),
+            Item(11, "name11", "description11"),
+            Item(12, "name12", "description12"),
+            Item(13, "name13", "description13"),
+            Item(14, "name14", "description14"),
+            Item(15, "name15", "description15"),
+            Item(16, "name16", "description16"),
+            Item(17, "name17", "description17"),
+            Item(18, "name18", "description18"),
+            Item(19, "name19", "description19")
         )
         recyclerviewMain.apply {
             linerLayoutManager = LinearLayoutManager(activity)
@@ -48,7 +72,37 @@ class FirstScreen : Fragment(), MainAdapter.OnItemClickListener {
     }
 
     override fun onItemClick(position: Int) {
-        activity?.getSupportFragmentManager()?.beginTransaction()
+        //val bundle = Bundle()
+        //bundle.putString("id", (item_list_main[position].id).toString())
+        //bundle.putString("name", item_list_main[position].name)
+        //bundle.putString("description", item_list_main[position].description)
+        //val second_fragment = SecondScreen()
+        //second_fragment.arguments = bundle
+
+        val id_item = (item_list_main[position].id).toString()
+        val name_item = item_list_main[position].name
+        val description_item = item_list_main[position].description
+
+        shared_id = FragmentActivity().getSharedPreferences("pref_id", Context.MODE_PRIVATE)
+        shared_name = FragmentActivity().getSharedPreferences("pref_name", Context.MODE_PRIVATE)
+        shared_description = FragmentActivity().getSharedPreferences("pref_description", Context.MODE_PRIVATE)
+        var edit_id = shared_id.edit()
+        var edit_name = shared_name.edit()
+        var edit_description = shared_description.edit()
+        edit_id.putString("id_shared", id_item)
+        edit_name.putString("name_shared", name_item)
+        edit_description.putString("description_shared", description_item)
+        edit_id.commit()
+        edit_name.commit()
+        edit_description.commit()
+
+        //communicator = requireActivity() as Communicator
+        //communicator.passDataCom(id_item,
+            //name_item,
+            //description_item)
+        //fragmentManager?.beginTransaction()?.replace(R.id.main_frame, SecondScreen())?.commit()
+        activity?.getSupportFragmentManager()
+            ?.beginTransaction()
             ?.replace(R.id.main_frame, SecondScreen.newInstance())
             ?.commit()
     }
--- app/src/main/java/com/klinovvlad/task1klinov/Fragments/SecondScreen.kt
+++ app/src/main/java/com/klinovvlad/task1klinov/Fragments/SecondScreen.kt
@@ -1,20 +1,59 @@
 package com.klinovvlad.task1klinov.Fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentResultListener
 import com.klinovvlad.task1klinov.R
+import com.klinovvlad.task1klinov.databinding.SecondScreenBinding
 
 class SecondScreen : Fragment() {
+    private lateinit var second_screen_binding: SecondScreenBinding
+    var id_string: String? = null
+    var name_string: String? = null
+    var description_string: String? = null
+
+    lateinit var shared_id : SharedPreferences
+    lateinit var shared_name : SharedPreferences
+    lateinit var shared_description : SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_second_screen, container, false)
+        second_screen_binding = SecondScreenBinding.inflate(inflater, container, false)
+
+        shared_id = FragmentActivity().getSharedPreferences("pref_id", Context.MODE_PRIVATE)
+        shared_name = FragmentActivity().getSharedPreferences("pref_name", Context.MODE_PRIVATE)
+        shared_description = FragmentActivity().getSharedPreferences("pref_description", Context.MODE_PRIVATE)
+        second_screen_binding.textViewSecond1.text = shared_id.getString("id_shared", "")
+
+
+        //Toast.makeText(activity, arguments?.getString("name"), Toast.LENGTH_SHORT).show()
+
+        //id_string = arguments?.getString("id")
+        //name_string = arguments?.getString("name")
+        //description_string = arguments?.getString("description")
+        //second_screen_binding.textViewSecond1.text = id_string
+        //second_screen_binding.textViewSecond2.text = name_string
+        //second_screen_binding.textViewSecond3.text = description_string
+
+        //val args = this.arguments
+        //val id_data = args?.getString("id")
+        //val name_data = args?.getString("name")
+        //val description_data = args?.getString("description")
+        //second_screen_binding.textViewSecond1.text = id_data.toString()
+        //second_screen_binding.textViewSecond2.text = name_data.toString()
+        //second_screen_binding.textViewSecond3.text = description_data.toString()
+        return second_screen_binding.root
     }
+
 
     companion object {
         @JvmStatic
